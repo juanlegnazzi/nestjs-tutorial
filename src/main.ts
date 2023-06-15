0f5904b4b0d16308a7c7bea63ee38e4095a362ad
Juan Legnazzi <jflegnazzi@gmail.com>
2023-06-15 11:18:35 -0300

    Creacion de resource 'items' e instalacion de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Mi API documentation')
+  .setDescription('Tutorial de NestJS')
+  .setVersion('1.0')
+  .addTag('items')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
