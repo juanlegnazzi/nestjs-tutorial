3aa8bf6f6209c7547ef7bb9fb31339f6addecb42
Juan Legnazzi <jflegnazzi@gmail.com>
2023-06-15 11:53:47 -0300

    Implemenmtacion del items create

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Items, ItemsDocument } from './schema/items.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ItemsService {
-  create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+  //Injectamos el modelo de items y se lo estamos asignando a la propiedad itemsModule
+  constructor(
+    @InjectModel(Items.name) private itemsModule:Model<ItemsDocument>
+    ) {}
+
+  async create(createItemDto: CreateItemDto) {
+    //TODO DTO createItemDto --> BODY esto trae la data
+    const itemsCreated = await this.itemsModule.create(createItemDto)
+    return itemsCreated;
   }
 
   findAll() {
